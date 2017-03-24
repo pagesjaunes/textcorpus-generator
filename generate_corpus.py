ab9570e6a356d96909bd5cc1f837a550399f1e41
Fabrice Depaulis <fdepaulis@pagesjaunes.fr>
2017-03-24 11:58:14 +0100

    main executable

---
+++ generate_corpus.py
@@ -0,0 +1,31 @@
+"""
+    Text corpus generation
+
+Usage:
+    generate_corpus.py --template=filepath --output_path=dirpath [--debug]
+
+Options:
+    --help                  Displays help message
+    --version               Displays the version number
+    --template=filepath     Sets the path to the template file
+    --output_path=filepath  Sets the path where to write the generated corpus
+    --debug                 Debug version of the script
+
+"""
+
+from docopt import docopt
+from textcorpus_generator.commons import configuration, logger
+
+if __name__ == '__main__':
+    conf = configuration.load()
+
+    # Command line args
+    # __doc__ contains the module docstring
+    arguments = docopt(__doc__, version=conf['version'])
+
+    if arguments['--debug']:
+        conf['log']['level'] = 'DEBUG'
+
+    logger.configure(conf['log']['level_values'][conf['log']['level']], conf['log']['dir'], conf['log']['filename'], conf['log']['max_filesize'], conf['log']['max_files'])
+
+    logger.info("It works !")
